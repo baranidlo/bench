16bf23b4a3d355a9379c323709445e5e4788bd2a
sleet01 <sleet01@uw.edu>
2018-01-25 12:25:28 -0800

    Automatic Tournament Update Commit 2018-01-25 12:25:27-08:00

--- tournament/refresh_recent_files.sh
+++ tournament/refresh_recent_files.sh
@@ -4,7 +4,7 @@ JUGGLERFOLDER="http://lists.starwarsclubhouse.com/api/v1/tournament/"
 COUNT=${1:-100}
 
 # Find the 100 most recently added files and refresh them.
-for FILE in `find . -maxdepth 1 -type f -regextype sed -regex "./[0-9]\{1,\}" -printf "%T+\t%p\n" | sort -r | cut -f 2 | head -${COUNT}`; 
+for FILE in `find . -maxdepth 1 -type f -regextype sed -regex "./[0-9]\{1,\}" -printf "%T+\t%p\n" | sort -k1 | cut -f 2 | tail -${COUNT}`; 
 do 
 	# Ideally this would only refresh files if they are newer than the local copy;
 	# because list juggler does not have mtime data turned on, in practice this
